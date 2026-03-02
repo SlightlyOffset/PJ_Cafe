e7e7e60b989befa52fc9e294c6debbfc81eef7d6
White <69239449+SlightlyOffset@users.noreply.github.com>
2026-03-02 19:50:34 +0700

    feat(core): Add `Grid` class to manage tile-based grid system
    - Includes methods for grid initialization, tile retrieval, and random tile setup for testing purposes.
    
    Signed-off-by: White <69239449+SlightlyOffset@users.noreply.github.com>

---
+++ core/src/main/java/core/Grid.java
@@ -0,0 +1,45 @@
+package core;
+
+import java.util.Random;
+
+public class Grid {
+    private final int cols;
+    private final int rows;
+    private Tile[][] tiles;
+
+    public Grid(int cols, int rows) {
+        this.cols = cols;
+        this.rows = rows;
+        tiles = new Tile[rows][cols];
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public Tile[][] getTiles() {
+        return tiles;
+    }
+
+    public void setTiles(Tile[][] tiles) {
+        this.tiles = tiles;
+    }
+
+    // Randomly initialize tiles <-- for testing purposes
+    public void randomInitTile() {
+        TileType[] types = TileType.values();
+        Random random = new Random();
+        for (int y=0; y<rows; y++) {
+            for (int x=0; x<cols; x++) {
+                Tile tile = new Tile(types[random.nextInt(types.length)]);
+                for (int r=0; r< random.nextInt(4); r++) {tile.rotateClockwise();}
+                tiles[y][x] = tile;
+            }
+        }
+    }
+
+}
