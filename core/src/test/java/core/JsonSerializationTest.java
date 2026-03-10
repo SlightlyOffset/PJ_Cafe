7f1a0fcdb58deb824ed2f4e5cddb00594e5c4e2d
White <69239449+SlightlyOffset@users.noreply.github.com>
2026-03-10 23:30:03 +0700

    test(mechanics): add thorough unit tests for Grid serialization

--- core/src/test/java/core/JsonSerializationTest.java
+++ core/src/test/java/core/JsonSerializationTest.java
@@ -2,6 +2,7 @@ package core;
 
 import com.badlogic.gdx.utils.Json;
 import core.mechanics.Grid;
+import core.mechanics.LevelLoader;
 import core.mechanics.Tile;
 import core.mechanics.TileType;
 import org.junit.jupiter.api.Test;
@@ -24,21 +25,29 @@ public class JsonSerializationTest {
 
     @Test
     public void testGridSerialization() {
-        Json json = new Json();
         Grid grid = new Grid(2, 2);
         Tile[][] tiles = new Tile[2][2];
         tiles[0][0] = new Tile(TileType.STRAIGHT);
+        tiles[0][0].rotateClockwise(); // 90
         tiles[0][1] = new Tile(TileType.L_TURN);
+        tiles[0][1].rotateClockwise(); // 90
+        tiles[0][1].rotateClockwise(); // 180
         tiles[1][0] = new Tile(TileType.T_JUNCTION);
         tiles[1][1] = new Tile(TileType.CROSS);
         grid.setTiles(tiles);
 
-        String jsonString = json.toJson(grid);
-        Grid deserializedGrid = json.fromJson(Grid.class, jsonString);
+        String jsonString = LevelLoader.toJson(grid);
+        Grid deserializedGrid = new Json().fromJson(Grid.class, jsonString);
 
         assertEquals(grid.getCols(), deserializedGrid.getCols());
         assertEquals(grid.getRows(), deserializedGrid.getRows());
         assertNotNull(deserializedGrid.getTiles());
-        assertEquals(grid.getTiles()[0][0].getType(), deserializedGrid.getTiles()[0][0].getType());
+        
+        for (int y = 0; y < 2; y++) {
+            for (int x = 0; x < 2; x++) {
+                assertEquals(grid.getTiles()[y][x].getType(), deserializedGrid.getTiles()[y][x].getType());
+                assertEquals(grid.getTiles()[y][x].getRotation(), deserializedGrid.getTiles()[y][x].getRotation());
+            }
+        }
     }
 }
